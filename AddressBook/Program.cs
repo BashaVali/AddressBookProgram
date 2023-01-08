05d5a1ecfd758c76eba7beefc2acf2ec46f04849
BashaVali <bashavali222@gmail.com>
2023-01-09 03:34:59 +0530

    Address Book Program

---
+++ AddressBook/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+            Contact contact = new Contact() //object creation
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Convert.ToInt64(Console.ReadLine()),
+                PhoneNUmber = Console.ReadLine(),
+                Email = Console.ReadLine()
+
+            };
+            Console.WriteLine("Contact Details:" + "\n" + "FirstName: " + contact.FirstName + "\n" + "LastName: " + contact.LastName + "\n" + "Address: " + contact.Address + "\n" + "City: " + contact.City + "\n" + "State: " + contact.State + "\n" + "Zip: " + contact.Zip + "\n" + "PhoneNumber: " + contact.PhoneNUmber + "\n" + "Email: " + contact.Email);
+
+        }
+    }
+}
